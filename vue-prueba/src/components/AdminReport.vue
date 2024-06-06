5e52fd9df2eca67d8ac0f0c805e21441afae6a7b
aylenlee <aylenteresalee@gmail.com>
2024-06-05 23:09:11 -0300

    admin report + paquete bootstrap + footer

--- vue-prueba/src/components/AdminReport.vue
+++ vue-prueba/src/components/AdminReport.vue
@@ -1,10 +1,60 @@
 <template>
-
+    <div>
+    <h2>Lista de Pacientes</h2>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nombre del paciente</th>
+          <th>Mail</th>
+          <th>Especie</th>
+          <th>Raza</th>
+          <th>Sexo</th>
+          <th>Edad</th>
+          <th>Nombre del propietario</th>
+          <th>Historial clínico</th>
+          <th>Fecha de creación</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td>{{ user.id }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.species }}</td>
+          <td>{{ user.breed }}</td>
+          <td>{{ user.sex }}</td>
+          <td>{{ user.age }}</td>
+          <td>{{ user.ownerName }}</td>
+          <td>{{ user.historialClinico }}</td>
+          <td>{{ user.createdAt }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const users = ref([]);
+
+const fetchUsers = async () => {
+  try {
+    const response = await axios.get('https://666112b663e6a0189fe85cb5.mockapi.io/users');
+    users.value = response.data;
+  } catch (error) {
+    console.error('Error al obtener los usuarios');
+  }
+};
+
+onMounted(() => {
+  fetchUsers();
+});
 
 </script>
+
 
 <style scoped>
 
--- vue-prueba/src/views/AdminReportView.vue
+++ vue-prueba/src/views/AdminReportView.vue
@@ -1,12 +1,13 @@
 <template>
-    <div>
-        <h1>Admin Report</h1>
-        <P>Visible solo para administradores</P>
+    <div class="text-center">
+        <h1>Informe para administradores</h1>
+        <p>Visible solo para administradores</p>
     </div>
+    <AdminReport />
 </template>
 
 <script setup>
-
+import AdminReport from '../components/AdminReport.vue';
 </script>
 
 <style scoped>
